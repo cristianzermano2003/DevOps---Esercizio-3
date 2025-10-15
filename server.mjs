446256b4adc4022b4ac0f71c6a6f3063f5517ba9
Cristian Zermano <cristian.zermanowm@gmail.com>
2025-10-15 12:11:02 +0200

    fourth commit

--- server.mjs
+++ server.mjs
@@ -6,9 +6,9 @@ const server = createServer((req, res) => {
   res.end('Ciao Mondo!\n');
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+// starts a simple http server on port 3000
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Listening on 0.0.0.0:3000');
 });
 
 // run with `node server.mjs`
